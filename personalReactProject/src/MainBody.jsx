1f05b637266117e680eabff61f72ed7bc26998b8
Alexander Söderwall <alexander.soderwall@gmail.com>
2024-03-28 18:56:23 +0100

    Can go between two columns.

--- personalReactProject/src/MainBody.jsx
+++ personalReactProject/src/MainBody.jsx
@@ -5,6 +5,7 @@ import RenderCards from "./RenderCards";
 
 const MainBody = () => {
   const [cards, setCards] = useState([]);
+  const [doingCards, setDoingCards] = useState([]);
 
   const handleDragOver = (event) => {
     event.preventDefault();
@@ -12,34 +13,31 @@ const MainBody = () => {
 
   const handleDrop = (event) => {
     event.preventDefault();
-    const cardId = event.dataTransfer.getData("text/plain");
-    const cardIndex = cards.findIndex((card) => card.id === cardId);
-    if (cardIndex !== -1) {
-      console.log("Enter?");
-      const newCards = [...cards];
-      const removedCard = newCards.splice(cardIndex, 1)[0];
-      setCards(newCards);
-    }
+    const cardId = JSON.parse(event.dataTransfer.getData("text/plain"));
+    const movedCard = cards.find((card) => card.id === cardId);
+    const removeFromCards = cards.filter((card) => card.id !== cardId);
+    setCards(removeFromCards);
+    setDoingCards((prevDoingCards) => [...doingCards, movedCard]);
+    console.log(doingCards);
+    console.log(cards);
   };
 
   return (
     <>
       <main className="MainBody">
-        <div
-          className="MainBodyDivCommon"
-          onDragOver={handleDragOver}
-          onDrop={handleDrop}
-        >
+        <div className="MainBodyDivCommon">
           Tasks
           <RenderCards cards={cards} setCards={setCards} />
           <AddTaskButton cards={cards} setCards={setCards} />
         </div>
         <div
           className="MainBodyDivCommon"
+          id="doingDiv"
           onDragOver={handleDragOver}
           onDrop={handleDrop}
         >
           Doing
+          <RenderCards cards={doingCards} setCards={setDoingCards} />
         </div>
         <div className="MainBodyDivCommon">Done</div>
       </main>
